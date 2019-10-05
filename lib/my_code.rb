cb6aca9a8bacf9612c2c083e5c83ad293e55b72b
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-05 01:22:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,9 +17,13 @@ end
 def reduce_to_all_true(source_array)
   i = 0
  while i < source_array.length do
-   if source_array[i] = true
+   if source_array[i]
      return true
    else
      return false
  end
+
 end
+#def reduce_to_any_true(source_array)
+#
+#end
